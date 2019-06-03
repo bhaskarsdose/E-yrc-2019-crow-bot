36c117848c2412725b52db2b57693808964790bf
Akshay <akshaysharma9211321137@gmail.com>
2019-06-04 02:01:30 +0530

    Other Files

---
+++ config.py
@@ -0,0 +1,49 @@
+arena_config ={0:("Water pitcher",6,"2-2") ,1:("Pebble",8,"3-3") ,3:("Pebble",16,"2-2") ,5:("Pebble",19,"1-1")}
+
+Robot_start = "Start-1"
+
+arena_config ={0:("Water pitcher",7,"3-3") ,1:("Pebble",1,"2-2") ,3:("Pebble",14,"1-1") ,5:("Pebble",15,"3-3")}
+
+Robot_start = "Start-2"
+
+arena_config ={0:("Water pitcher",9,"2-2") ,1:("Pebble",2,"3-3") ,3:("Pebble",16,"2-2") ,5:("Pebble",17,"1-1")}
+
+Robot_start = "Start-1"
+arena_config ={0:("Water pitcher",13,"1-1") ,1:("Pebble",9,"3-3") ,3:("Pebble",3,"2-2") ,5:("Pebble",11,"1-1")}
+
+Robot_start = "Start-2"
+arena_config ={0:("Water pitcher",10,"2-2") ,1:("Pebble",5,"1-1") ,3:("Pebble",3,"3-3") ,5:("Pebble",17,"2-2")}
+
+Robot_start = "Start-1"
+#### bakchod config1
+arena_config ={0:("Water pitcher",9,"2-2") ,1:("Pebble",5,"3-3") ,3:("Pebble",10,"2-2") ,5:("Pebble",15,"1-1")}
+
+Robot_start = "Start-2"
+#### bakchod config2
+arena_config ={0:("Water pitcher",5,"2-2") ,1:("Pebble",1,"3-3") ,3:("Pebble",7,"1-1") ,5:("Pebble",18,"3-3")}
+
+Robot_start = "Start-1"
+###deepbakchod
+arena_config ={0:("Water pitcher",19,"3-3") ,6:("Pebble",1,"2-2") ,2:("Pebble",16,"3-3") ,4:("Pebble",6,"1-1")}
+
+Robot_start = "Start-2"
+###deepbakchod2
+arena_config ={0:("Water pitcher",18,"2-2") ,2:("Pebble",17,"2-2") ,6:("Pebble",4,"3-3") ,4:("Pebble",7,"1-1")}
+
+Robot_start = "Start-2"
+arena_config ={0:("Water pitcher",6,"2-2") ,1:("Pebble",8,"3-3") ,3:("Pebble",16,"2-2") ,5:("Pebble",19,"1-1")}
+
+Robot_start = "Start-1"
+arena_config ={0:("Water pitcher",6,"2-2") ,1:("Pebble",8,"3-3") ,3:("Pebble",16,"2-2") ,5:("Pebble",19,"1-1")}
+
+Robot_start = "Start-1"
+arena_config ={0:("Water pitcher",5,"1-1") ,2:("Pebble",19,"2-2") ,6:("Pebble",14,"3-3") ,4:("Pebble",16,"2-2")}
+
+Robot_start = "Start-2"
+arena_config ={0:("Water pitcher",7,"1-1") ,3:("Pebble",5,"1-1") ,2:("Pebble",14,"2-2") ,5:("Pebble",17,"2-2")}
+
+Robot_start = "Start-1"
+
+arena_config ={0:("Water pitcher",5,"2-2") ,3:("Pebble",7,"3-3") ,5:("Pebble",13,"2-2")}
+
+Robot_start = "Start-2"
